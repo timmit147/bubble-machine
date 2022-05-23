c33bfb8f6ebcd10ac68373a74982079530db3e00
timmit147 <timmeeuwsen@hotmail.nl>
2022-05-23 17:29:56 +0200

    Working nodes

--- js/script.js
+++ js/script.js
@@ -2,6 +2,16 @@ var
   svg = document.getElementById('mysvg'),
   NS = svg.getAttribute('xmlns');
 
+  
+// add random circles
+// c = document.createElementNS(NS, 'circle');
+// c.setAttributeNS(null, 'cx', 0);
+// c.setAttributeNS(null, 'cy',  0);
+// c.setAttributeNS(null, 'r', 0.04);
+// svg.appendChild(c);
+
+
+
 function httpGet(){
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open( "GET", "https://bubble-machine-api-dummy.herokuapp.com/rest/session/2", false ); // false for synchronous request
@@ -14,6 +24,14 @@ const session = JSON.parse(httpGet());
 console.log(session);
 
 
+function reversNumber(number){
+if(number < 0){
+    return Math.abs(number);
+}
+else{
+    return Math.abs(number) * -1;
+}
+}
 
 var i = 0;
 for (const item of session["nodes"] ) {
@@ -21,8 +39,8 @@ for (const item of session["nodes"] ) {
     li.appendChild(document.createTextNode(session["nodes"][i]["id"]+" | "+ session["nodes"][i]["label"]+" | X="+session["nodes"][i]["x"]+" | Y="+session["nodes"][i]["y"]));
     document.querySelector("#listNotes").appendChild(li);
     c = document.createElementNS(NS, 'circle');
-c.setAttributeNS(null, 'cx', session["nodes"][i]["x"]/2+1);
-c.setAttributeNS(null, 'cy',  session["nodes"][i]["y"]/2+1);
+c.setAttributeNS(null, 'cx', session["nodes"][i]["x"]);
+c.setAttributeNS(null, 'cy',  reversNumber(session["nodes"][i]["y"]));
 c.setAttributeNS(null, 'r', 0.04);
 svg.appendChild(c);
     i++;
@@ -31,22 +49,14 @@ svg.appendChild(c);
 
 
 
-// var i = 0;
-// for (const item of session["links"] ) {
-//     var li = document.createElement("li");
-//     li.appendChild(document.createTextNode(session["links"][i]["label"]+" - "+session["links"][i]["source"]+" - "+session["links"][i]["target"]));
-//     document.querySelector("#listDocuments").appendChild(li);
-//     i++;
-// }
+var i = 0;
+for (const item of session["links"] ) {
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(session["links"][i]["label"]+" - "+session["links"][i]["source"]+" - "+session["links"][i]["target"]));
+    document.querySelector("#listDocuments").appendChild(li);
+    i++;
+}
 
 // https://www.sitepoint.com/how-to-translate-from-dom-to-svg-coordinates-and-back-again/
 
-
-  
-// add random circles
-//   c = document.createElementNS(NS, 'circle');
-//   c.setAttributeNS(null, 'cx', 1);
-//   c.setAttributeNS(null, 'cy',  1);
-//   c.setAttributeNS(null, 'r', 0.04);
-//   svg.appendChild(c);
 
